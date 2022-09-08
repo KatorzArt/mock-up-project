d955d3739071b4ffaff0864099a8c63fb862a961
Katorz <katorz.art@gmail.com>
2022-09-08 22:12:45 +0200

    Change styling setup and create basic settings markup

--- src/components/MuSettings.vue
+++ src/components/MuSettings.vue
@@ -1,7 +1,13 @@
 <template>
     <div class="settings">
-        <h2>Settings</h2>
-        <hr>
+        <div>
+            <label for="model">Model</label>
+            <input type="file" name="model" id="model" accept=".fbx" />
+        </div>
+        <div>
+            <label for="texture">Texture</label>
+            <input type="file" name="texture" id="texture" accept=".png" />
+        </div>
     </div>
 </template>
 
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { createApp } from "vue";
-import "./style.css";
+import "./style/reset.css";
+import "./style/main.css";
 import App from "./App.vue";
 
 createApp(App).mount("body");
